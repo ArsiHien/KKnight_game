9af9040473b57cd1963cd98cbf9a655ff2df0318
duchien714 <duchien7124@gmail.com>
2023-04-18 16:26:44 +0700

    Update Monster

--- Enemy.cpp
+++ Enemy.cpp
@@ -5,13 +5,24 @@ Monster::Monster()
     HP = 150;
     on_ground = false;
     frame = 0;
-    mBox = {0, 0, 60, 60};
-    x_pos = 0;
-    y_pos = 0;
+    status = LEFT;
+    mBox = {0, 0, 96, 96};
+    input_type.left = 0;
+    input_type.right = 0;
+    //x_pos = 0; y_pos = 0;
     mVelX = 0;
     mVelY = 0;
     map_x = 0;
     map_y = 0;
+    x_min = 0;
+    x_max = 0;
+    isDead = false;
+
+    animations.push_back(idle);
+    animations.push_back(run);
+    animations.push_back(attack);
+    animations.push_back(hurt);
+    animations.push_back(dead);
 }
 
 Monster::~Monster()
@@ -21,38 +32,134 @@ Monster::~Monster()
 
 void Monster::loadMonsterTexture(RenderWindow &window)
 {
-    mTexture = window.loadIMG("gfx/threat_level.png");
+    mTexture = window.loadIMG("gfx/Sprite_monsterS.png");
     if(mTexture == nullptr)
     {
-        cout << "Failed to load player texture\n";
+        cout << "Failed to load monster texture\n";
     }
 }
 
 void Monster::set_clips()
 {
-    for(int i = 0; i < MONSTER_FRAME_NUM; i++)
+    int n = 0;
+    for(int i = 0; i < (int)animations.size(); i++)
     {
-        frame_clip[i].x = i*MONSTER_WIDTH;
-        frame_clip[i].y = 0;
-        frame_clip[i].w = MONSTER_WIDTH;
-        frame_clip[i].h = MONSTER_HEIGHT;
+        for(int j = 0; j < animations[i].amount_of_frame; j++)
+        {
+            Monster_Clips[n].x = j*animations[i].frame_width;
+            Monster_Clips[n].y = (i+1)*144 - animations[i].frame_height;
+            Monster_Clips[n].h = animations[i].frame_height;
+            Monster_Clips[n].w = animations[i].frame_width;
+            n++;
+        }
     }
+}
+
+void Monster::set_x_pos(const int &xp)
+{
+    mBox.x = xp;
+    x_min = xp - 100;
+    x_max = xp + 100;
 }
 
 void Monster::show(RenderWindow &window)
 {
-    mBox.x = x_pos - map_x;
-    mBox.y = y_pos - map_y;
-    frame++;
-    if(frame >= 8)
+    int dxr = 0, dxl = 0, dy = 0, slow = 1, t = 0;
+//    mBox.x = mBox.x;
+//    mBox.y = mBox.y;
+    currentBox.x = mBox.x - map_x;
+    currentBox.y = mBox.y - map_y;
+    if(HP <= 0)
     {
-        frame = 0;
+        dxr = dead.denta_x_right, dxl = dead.denta_x_left, dy = dead.denta_y;
+        slow = dead.slow_down;
+        currentBox.w = dead.frame_width;
+        currentBox.h = dead.frame_height;
+        for(int i = 0; i <dead.index; i++)
+        {
+            t += animations[i].amount_of_frame;
+        }
+        frame++;
+        if(frame >= dead.amount_of_frame*slow)
+        {
+            frame = 0;
+            isDead = true;
+        }
     }
-    SDL_Rect* current_clip = &frame_clip[frame];
-    window.render(mTexture, mBox.x, mBox.y, mBox.w, mBox.h, current_clip);
+    else if(input_type.hurt == 1)
+    {
+        dxr = hurt.denta_x_right, dxl = hurt.denta_x_left, dy = hurt.denta_y;
+        slow = hurt.slow_down;
+        currentBox.w = hurt.frame_width;
+        currentBox.h = hurt.frame_height;
+        for(int i = 0; i <hurt.index; i++)
+        {
+            t += animations[i].amount_of_frame;
+        }
+        frame++;
+        if(frame >= hurt.amount_of_frame*slow)
+        {
+            frame = 0;
+            input_type.hurt = 0;
+        }
+    }
+    else if(input_type.attack == 1)
+    {
+        dxr = attack.denta_x_right, dxl = attack.denta_x_left, dy = attack.denta_y;
+        slow = attack.slow_down;
+        currentBox.w = attack.frame_width;
+        currentBox.h = attack.frame_height;
+        for(int i = 0; i <attack.index; i++)
+        {
+            t += animations[i].amount_of_frame;
+        }
+        frame++;
+        if(frame >= attack.amount_of_frame*slow)
+        {
+            frame = 0;
+            input_type.attack = 0;
+        }
+    }
+    else if(input_type.run == 1)
+    {
+        dxr = run.denta_x_right, dxl = run.denta_x_left, dy = run.denta_y;
+        slow = run.slow_down;
+        currentBox.w = run.frame_width;
+        currentBox.h = run.frame_height;
+        for(int i = 0; i <run.index; i++)
+        {
+            t += animations[i].amount_of_frame;
+        }
+        frame = ++frame%(run.amount_of_frame*slow);
+    }
+    else
+    {
+        dxr = idle.denta_x_right, dxl = idle.denta_x_left, dy = idle.denta_y;
+        slow = idle.slow_down;
+        currentBox.w = idle.frame_width;
+        currentBox.h = idle.frame_height;
+        for(int i = 0; i <idle.index; i++)
+        {
+            t += animations[i].amount_of_frame;
+        }
+        frame++;
+        if(frame >= idle.amount_of_frame*slow)
+        {
+            frame = 0;
+        }
+    }
+    SDL_Rect* current_clip = &Monster_Clips[t + frame/slow];
+    if(status == LEFT)
+    {
+        window.render(mTexture, currentBox.x - dxl, currentBox.y - dy, currentBox.w, currentBox.h, current_clip);
+    }
+    else
+    {
+        window.render(mTexture, currentBox.x - dxr, currentBox.y - dy, currentBox.w, currentBox.h, current_clip, 0.0, NULL, SDL_FLIP_HORIZONTAL);
+    }
 }
 
-void Monster::doPlayer(Tile* map_data)
+void Monster::move(SDL_Rect player_box, Tile* map_data)
 {
     mVelX = 0;
     mVelY += MONSTER_GRAVITY_SPEED;
@@ -60,33 +167,61 @@ void Monster::doPlayer(Tile* map_data)
     {
         mVelY = MONSTER_MAX_FALL_SPEED;
     }
+    input_type.run = 1;
+    input_type.right = 0;
+    input_type.left = 0;
+    int distance = sqrt(min(pow((x_min - player_box.x + player_box.w/2),2), pow((x_max - player_box.x + player_box.w/2),2)) + pow((mBox.y + mBox.h/2 - player_box.y + player_box.h/2), 2));
+    if(mBox.x >= x_min && mBox.x <= x_max && distance <= 256)
+    {
+        if(player_box.x + player_box.w/2 == mBox.x + mBox.w/2) return;
+        if(mBox.x == x_min || mBox.x == x_max) input_type.run = 0;
+        if(player_box.x + player_box.w/2 < mBox.x + mBox.w/2)
+        {
+            status = LEFT;
+            input_type.left = 1;
+            input_type.right = 0;
+        }
+        else if(player_box.x + player_box.w/2 > mBox.x + mBox.w/2)
+        {
+            status = RIGHT;
+            input_type.left = 0;
+            input_type.right = 1;
+        }
+    }
+    else if(distance > 256) input_type.run = 0;
+    if(input_type.attack == 1) mVelX = 0;
+    else if(input_type.left == 1)
+    {
+        mVelX = -MONSTER_SPEED;
+    }
+    else if(input_type.right == 1) mVelX = MONSTER_SPEED;
     checkToMap(map_data);
 }
 
 void Monster::checkToMap(Tile *map_data)
 {
-    x_pos += mVelX;
-    if( ( x_pos < 0 ) || ( x_pos + MONSTER_WIDTH > LEVEL_WIDTH ) || touchesWall( map_data ) )
+    mBox.x += mVelX;
+    if( ( mBox.x < 0 ) || (mBox.x < x_min) || (mBox.x > x_max) || ( mBox.x + mBox.w > LEVEL_WIDTH ) || touchesWall( map_data ) )
     {
-        x_pos -= mVelX;
+        mBox.x -= mVelX;
     }
-    y_pos += mVelY;
-    if( ( y_pos < 0 ) || ( y_pos + MONSTER_HEIGHT > LEVEL_HEIGHT ) || touchesWall( map_data)  )
+    mBox.y += mVelY;
+    mBox.y = mBox.y;
+    if( ( mBox.y < 0 ) || ( mBox.y + mBox.h > LEVEL_HEIGHT ) || touchesWall( map_data)  )
     {
-        y_pos -= mVelY;
+        mBox.y -= mVelY;
     }
 }
 
-bool Monster::checkCollision(SDL_Rect b )
+bool Monster::checkCollision( SDL_Rect a, SDL_Rect b)
 {
-    int leftA = x_pos, leftB = b.x;
-    int rightA = x_pos + MONSTER_WIDTH, rightB = b.x + b.w;
-    int topA = y_pos, topB = b.y;
-    int bottomA = y_pos + MONSTER_HEIGHT, bottomB = b.y + b.h;
+    int leftA = a.x, leftB = b.x;
+    int rightA = a.x + a.w, rightB = b.x + b.w;
+    int topA = a.y, topB = b.y;
+    int bottomA = a.y + a.h, bottomB = b.y + b.h;
 
     if( bottomA <= topB || topA >= bottomB || rightA <= leftB || leftA >= rightB)
     {
-        if(bottomA <= topB) on_ground = true;
         return false;
     }
     return true;
@@ -98,7 +233,7 @@ bool Monster::touchesWall(Tile *map_data)
     {
         if(map_data[i].getType() != -1)
         {
-            if( checkCollision(map_data[i].getBox()) )
+            if( checkCollision(mBox, map_data[i].getBox()) )
             {
                 return true;
             }
@@ -107,35 +242,65 @@ bool Monster::touchesWall(Tile *map_data)
     return false;
 }
 
-void Monster::isAttacked(Input i, SDL_Rect box, SDL_Rect currentBox, int status)
+void Monster::isAttacked(Input i, SDL_Rect player_box, SDL_Rect player_currentBox, int player_status)
 {
     if(i.attack1 == 1 || i.attack2 == 1 || i.attack3 == 1)
     {
-        if(status == RIGHT)
+        if(player_status == RIGHT)
         {
-            if(box.x < x_pos && checkCollision(currentBox))
+            if(player_box.x + player_box.w/2 < mBox.x + mBox.w/2 + 15 && checkCollision(mBox, player_currentBox))
             {
+                input_type.hurt = 1;
                 HP--;
             }
         }
-        if(status == LEFT)
+        if(player_status == LEFT)
         {
-            if(box.x >= x_pos && checkCollision(currentBox))
+            if(player_box.x + player_box.w/2 >= mBox.x + mBox.w/2 - 15 && checkCollision(mBox, player_currentBox))
             {
+                input_type.hurt = 1;
                 HP--;
             }
         }
     }
 }
 
-bool Monster::isAttacking(Input i, SDL_Rect box, int status)
+bool Monster::isAttacking(Input i, SDL_Rect player_box)
 {
-    if(i.defend == 0 )
+    SDL_Rect Attack_box = {mBox.x + mBox.w/2 - attack.frame_width/2, mBox.y + mBox.h - attack.frame_height, attack.frame_width, attack.frame_height};
+    if(checkCollision(Attack_box, player_box))
     {
-        if(checkCollision(box))
+        input_type.attack = 1;
+        if(input_type.hurt == 1) return false;
+        if(i.defend == 0)
         {
-            return true;
+            if(status == RIGHT)
+            {
+                if(player_box.x + 16 >= mBox.x + 32)
+                {
+                    return true;
+                }
+            }
+            if(status == LEFT)
+            {
+                if(player_box.x + 16 <= mBox.x + 160)
+                {
+                    return true;
+                }
+            }
         }
     }
     return false;
+}
+
+void Monster::drawHP(SDL_Renderer &ren, SDL_Rect &mCamera)
+{
+    if(HP <= 0) return;
+    SDL_Rect health_box = {mBox.x + mBox.w/2 - HP/10 - mCamera.x, mBox.y - 5 - mCamera.y, HP/5, 5};
+
+    SDL_SetRenderDrawColor(&ren, 255, 0, 0, 255);
+    SDL_RenderFillRect(&ren, &health_box);
+
+    SDL_SetRenderDrawColor(&ren, 0, 0, 255, 255);
+    SDL_RenderDrawRect(&ren, &health_box);
 }
--- Enemy.h
+++ Enemy.h
@@ -4,14 +4,12 @@
 #include "Header.h"
 #include "Tile.h"
 #include "RenderWindow.h"
-#include "Player.h"
 
-const int MONSTER_FRAME_NUM = 8;
+const int MONSTER_FRAME_NUM = 39;
+
+const int MONSTER_SPEED = 1;
 const int MONSTER_GRAVITY_SPEED = 1;
 const int MONSTER_MAX_FALL_SPEED = 8;
-
-const int MONSTER_WIDTH = 60;
-const int MONSTER_HEIGHT = 60;
 
 class Monster : public RenderWindow{
 public:
@@ -19,44 +17,38 @@ public:
     Monster();
     ~Monster();
 
-//    void set_x_val(const int& xVal) {x_val = xVal;}
-//    void set_y_val(const int& xVal) {x_val = xVal;}
-    void set_x_pos(const int& xp) {x_pos = xp;}
-    void set_y_pos(const int& yp) {y_pos = yp;}
-//
-//    int get_x_pos() const {return x_pos;}
-//    int get_y_pos() const {return y_pos;}
+    void set_x_pos(const int& xp);
+//    void set_y_pos(const int& yp) {y_pos = yp;}
     void setMapXY(const int& mp_x, const int& mp_y) {map_x = mp_x; map_y = mp_y;}
 
     void set_clips();
     void loadMonsterTexture(RenderWindow &window);
     void show(RenderWindow &window);
-//    int get_width_frame() const {return width_frame;}
-//    int get_height_frame() const {return height_frame;}
-    void doPlayer(Tile *map_data);
+    void move(SDL_Rect player_box, Tile* map_data);
     void checkToMap(Tile *map_data);
 
     bool touchesWall(Tile *map_data);
-    bool checkCollision(SDL_Rect b);
+    bool checkCollision(SDL_Rect a, SDL_Rect b);
 
-    vector <Monster*> makeThreatList(SDL_Renderer* ren);
+    //dwvector <Monster*> makeThreatList(SDL_Renderer* ren);
 
-    void isAttacked(Input i, SDL_Rect box, SDL_Rect currentBox, int status);
-    bool isAttacking (Input i, SDL_Rect box, int status);
-    int getHP() {return HP;}
+    void isAttacked(Input i, SDL_Rect player_box, SDL_Rect player_currentBox, int player_status);
+    bool isAttacking (Input i, SDL_Rect player_box);
+    void drawHP(SDL_Renderer &ren, SDL_Rect &mCamera);
+    bool getDead() {return isDead;}
 
     SDL_Rect getBox(){return mBox;}
 private:
     int HP;
     SDL_Rect mBox;
+    SDL_Rect currentBox;
     SDL_Texture *mTexture;
-    int mVelX;
-    int mVelY;
+    int mVelX, mVelY;
+    //int x_pos, y_pos;
+    int x_min, x_max;
+    int push;
 
-    int x_pos;
-    int y_pos;
-
-    SDL_Rect frame_clip[MONSTER_FRAME_NUM];
+    SDL_Rect Monster_Clips[MONSTER_FRAME_NUM];
     int frame;
 
     bool on_ground;
@@ -64,7 +56,16 @@ private:
     int map_x;
     int map_y;
 
-    vector <Monster*> threats_list;
+    //vector <Monster*> threats_list;
+    int status;
+    Input input_type;
+    bool isDead;
+    Animation idle = Animation(0, 8, 96, 96, 8);
+    Animation run = Animation(1, 8, 120, 96, 8, 0, 15);
+    Animation attack = Animation(2, 10, 216, 144, 8, 0, 108, 48);
+    Animation hurt = Animation(3, 3, 96, 96, 8);
+    Animation dead = Animation(4, 10, 96, 96, 8);
+    vector <Animation> animations;
 };
 
 #endif // ENEMY_H_INCLUDED
